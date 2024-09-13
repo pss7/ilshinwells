0a255e9ac6d9ba91d065cbb4f4c9d22186fdc21d
parksangseok <>
2024-09-14 01:19:49 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -1,23 +1,50 @@
 $(function () {
 
+
+    /* section03 - slick */
+    $('#section03 .technologySlideWrap .technologySlideBox').slick({
+        infinite: true,
+        variableWidth: true,
+        slidesToShow: 1,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1025,
+                settings: {
+                    variableWidth: false,
+                    slidesToShow: 1
+                }
+            },
+        ]
+    });
+
+    /* section03 - slick */
+    /*
+    $('#section03 .imgSlickWrap .imgSlick').slick({
+        infinite: true,
+        slidesToShow: 1,
+        arrows : false, 
+    });
+*/
+
     /* section04 - slick */
     $('#section04 .tidingsSlideWrap .slick').slick({
         infinite: true,
         variableWidth: true,
         slidesToShow: 3,
-        arrows : false, 
-        responsive: [ 
-            {  
-                breakpoint: 481, 
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 481,
                 settings: {
                     variableWidth: false,
-                    slidesToShow:1
-                } 
+                    slidesToShow: 1
+                }
             },
         ]
     });
 
 
-    
+
 
 });
