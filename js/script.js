d3a98beb5fbe62f44929d418702b72fd1977c309
parksangseok <>
2024-09-13 23:54:48 +0900

    commit

--- js/script.js
+++ js/script.js
@@ -1,12 +1,9 @@
 $(function () {
 
-    /* section04 - slick */
-    $('#section04 .tidingsSlideWrap .slick').slick({
-        infinite: false,
-        variableWidth: true,
-        slidesToShow: 3,
-    });
 
+
+
+    
 
     
 
